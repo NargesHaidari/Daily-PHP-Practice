3752f52db8e28ae8a928e32082d2dda32234d76e
NargesHaidari <nargeshaidari2026@hotmail.com>
2026-09-14 07:28:37 +0500

    Learn Clousers in PHP

--- README.md
+++ README.md
@@ -31,6 +31,7 @@ This repository documents my daily PHP learning journey, including practice exer
 | 2026-09-10 | Date and Time in PHP                                   |
 | 2026-09-12 | JSON and Anonymous Funciton in PHP                     |
 | 2026-09-13 | PHP File Upload with Validation                        |
+| 2026-09-14 | Clousers in PHP                                        |
 
 ---
 
--- index.php
+++ index.php
@@ -1,6 +1,52 @@
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Name: <input type="text" name='name'><br><br>
-    File: <input type="file" name='my_file'><br><br>
-    <button type='submit'>Submit</button>
-</form>
+
+<?php
+
+$name = "Narges";
+$count = 0;
+
+$numbers = [1, 2, 3, 4];
+
+
+// 1. simple Closure
+$greet = function($name) {
+    return "Hello $name";
+};
+
+echo $greet("Narges") . "<br>";
+
+
+// 2. Closure with use
+$message = "Welcome";
+
+$welcome = function($name) use ($message) {
+    return "$message, $name";
+};
+
+echo $welcome($name) . "<br>";
+
+
+// 3. Closure with Reference
+$increase = function() use (&$count) {
+    $count++;
+};
+
+$increase();
+$increase();
+
+echo "Count: $count<br>";
+
+
+// 4. Closure as Callback
+$result = array_map(function($number) {
+    return $number * 2;
+}, $numbers);
+
+print_r($result);
+echo "<br>";
+
+
+// 5. Arrow Function
+$result2 = array_map(fn($number) => $number * 2, $numbers);
+
+print_r($result2);
