0679ead3a2dd1d334f7d9096f7d5139445509f06
NargesHaidari <nargeshaidari2026@hotmail.com>
2026-09-18 20:50:44 +0500

    Learn Objcet and Class in PHP

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ This repository documents my daily PHP learning journey, including practice exer
 | 2026-09-14 | Clousers in PHP                                        |
 | 2026-09-15 | Arrow Function and Foreach in PHP                      |
 | 2026-09-16 | Array Destructuring in PHP                             |
+| 2026-09-18 | Objects and Classes in PHP                             |
 
 ---
 
--- index.php
+++ index.php
@@ -1,19 +1,61 @@
 
+
 <?php
 
-$students = [
-    ["Narges", 17, "PHP"],
-    ["Ali", 19, "JavaScript"],
-    ["Sara", 18, "Python"],
-    ["Ahmad", 20, "Laravel"]
-];
+class Student
+{
+    public $name;
+    public $age;
+    public $grade;
+    public $favoriteSubject;
 
-echo "<h2>Student Information</h2>";
+    public function introduce()
+    {
+        echo "My name is " . $this->name . "<br>";
+        echo "I am " . $this->age . " years old.<br>";
+        echo "I am in grade " . $this->grade . ".<br>";
+        echo "My favorite subject is " . $this->favoriteSubject . ".<br>";
+    }
 
-foreach ($students as [$name, $age, $course]) {
+    public function study()
+    {
+        echo $this->name . " is studying " . $this->favoriteSubject . ".<br>";
+    }
 
-    echo "Name: $name <br>";
-    echo "Age: $age <br>";
-    echo "Course: $course <br>";
-    echo "<hr>";
+    public function birthday()
+    {
+        $this->age++;
+        echo $this->name . " is now " . $this->age . " years old.<br>";
+    }
 }
+
+
+// Object 1
+$student1 = new Student();
+
+$student1->name = "Narges";
+$student1->age = 17;
+$student1->grade = 10;
+$student1->favoriteSubject = "Computer Science";
+
+$student1->introduce();
+$student1->study();
+$student1->birthday();
+
+echo "<hr>";
+
+
+// Object 2
+$student2 = new Student();
+
+$student2->name = "Sara";
+$student2->age = 16;
+$student2->grade = 10;
+$student2->favoriteSubject = "Mathematics";
+
+$student2->introduce();
+$student2->study();
+$student2->birthday();
+
+?>
+
